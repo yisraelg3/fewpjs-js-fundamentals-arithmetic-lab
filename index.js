4b3d21ebbb8426d3fd792cbfc235bbeddb175d49
Yisrael Gurkow <yisrelg3@gmail.com>
2021-04-26 23:55:58 -0400

    commit tree 8bfe09351ca56947688b73b0a826b541b33d3daa

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+const num1 = 2, num2 = 31;
+const multiply = num1 *num2;
+
+const random = Math.ceil(Math.random() * 10);
+console.log(random);
+
+const num3 = 10, num4 = 6;
+const mod = num3 % num4;
+
+const max = Math.max(1, 3, 6, 9, 15, 20);
